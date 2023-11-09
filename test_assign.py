f3b9b8c122e8385d7abed5e5fe2a875bf8e64adf
Aadil Hussain <ault@andrims.com>
2023-11-09 16:58:24 -0500

    Update test_assign.py

--- test_assign.py
+++ test_assign.py
@@ -1,10 +1,13 @@
+'''
+ASSIGNMENT:
+multiply the input by 2
+use the parameter 'number' as an input
+return the answer as variable 'output'
+code your response in the function "submission"
+'''
 
 def submission(number):
-  # multiply the input by 2
-  # use the parameter 'number' as an input
-  # return the answer as variable 'output'
   output = int()
-  #  # code your submission here
-  
+  # code your submission here
   
   return output
