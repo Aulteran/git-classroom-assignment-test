c7e9c757a920111d9893a1c946919fcfaab7518c
Aadil Hussain <ault@andrims.com>
2023-11-09 16:57:16 -0500

    create pre-gen

---
+++ test_assign.py
@@ -0,0 +1,10 @@
+
+def submission(number):
+  # multiply the input by 2
+  # use the parameter 'number' as an input
+  # return the answer as variable 'output'
+  output = int()
+  #  # code your submission here
+  
+  
+  return output
